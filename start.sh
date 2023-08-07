df336c2a646bfeefed02b2af81ad00f46441b5f9
knterada5 <119117377+knterada5@users.noreply.github.com>
2023-08-08 05:28:58 +0900

    Add files via upload

--- start.sh
+++ start.sh
@@ -2,8 +2,13 @@
 
 if [ $# = 0 ]; then
   read -s -p "[sudo] password for $USER:" PSWD
+  print "\n"
+  read -p "Enter user name for git: " USER_NAME
+  read -p "Enter emai address for git: " USER_EMAIL
 else
   PSWD=$1
+  USER_NAME=$2
+  USER_EMAIL=$3
 fi
 
 # OS
@@ -18,7 +23,8 @@ case $OS in
       echo "Red Hat key"
     elif [[ $DIST =~ "debian" ]] || [[ $DIST =~ "ubuntu" ]]; then
       . $DIR/scripts/install_zsh_debian.sh $PSWD
-      zsh $DIR/scripts/install_packages_debian.sh $PSWD
+      . $DIR/scripts/install_fonts.sh $PSWD
+      zsh $DIR/scripts/install_packages_debian.sh $PSWD $USER_NAME $USER_EMAIL
       exit
     fi ;;
   "darwin")
