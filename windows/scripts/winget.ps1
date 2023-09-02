d7b463fd2c7e5509df70544fe6a025c36d1cd5c4
knterada5 <119117377+knterada5@users.noreply.github.com>
2023-09-02 10:09:52 +0900

    Create winget.ps1

---
+++ windows/scripts/winget.ps1
@@ -0,0 +1,73 @@
+# Install software via winget
+
+# Install sudo
+winget install --id gerardog.gsudo --accept-source-agreements --accept-package-agreements
+
+# Install iTunes
+winget install --id 9PB2MZ1ZMB1S --accept-source-agreements --accept-package-agreements
+
+# Install LINE
+winget install --id XPFCC4CD725961 --accept-source-agreements --accept-package-agreements
+
+# Install Wireless Adapter
+winget install --id 9WZDNCRFJBB1 --accept-source-agreements --accept-package-agreements
+
+# Install 7z
+winget install --id 7zip.7zip --accept-source-agreements --accept-package-agreements
+
+# Install JoyTokey
+winget install --id JTKsoftware.JoyToKey --accept-source-agreements --accept-package-agreements
+
+# Install Clip Studio
+winget install --id XPDP32QDDFL4PK --accept-source-agreements --accept-package-agreements
+
+# Install ESET Security
+winget install --id ESET.Security --accept-source-agreements --accept-package-agreements
+
+# Install Steam
+winget install --id Valve.Steam --accept-source-agreements --accept-package-agreements
+
+# Install Logi Options
+winget install --id Logitech.OptionsPlus --accept-source-agreements --accept-package-agreements
+
+# Install Remote mouse
+winget install --id EmoteInteractive.RemoteMouse --accept-source-agreements --accept-package-agreements
+
+# Install Virtual Box
+winget install --id Oracle.VirtualBox --accept-source-agreements --accept-package-agreements
+
+# Install Minecraft
+winget install --id Mojang.MinecraftLauncher --accept-source-agreements --accept-package-agreements
+
+# Install Android Studio
+winget install --id Google.AndroidStudio --accept-source-agreements --accept-package-agreements
+
+# Install Gimp
+winget install --id GIMP.GIMP --accept-source-agreements --accept-package-agreements
+
+# Install Inkscape
+winget install --id Inkscape.Inkscape --accept-source-agreements --accept-package-agreements
+
+# Install Thunderbird
+winget install --id Mozilla.Thunderbird --accept-source-agreements --accept-package-agreements
+
+# Install Blender
+winget install --id BlenderFoundation.Blender --accept-source-agreements --accept-package-agreements
+
+# Install Google Chrome
+winget install --id Google.Chrome --accept-source-agreements --accept-package-agreements
+
+# Install Vivaldi
+winget install --id VivaldiTechnologies.Vivaldi --accept-source-agreements --accept-package-agreements
+
+# Install VSCode
+winget install --id Microsoft.VisualStudioCode --accept-source-agreements --accept-package-agreements
+
+# Install Windows Terminal
+winget install --id Microsoft.WindowsTerminal --accept-source-agreements --accept-package-agreements
+
+# Install PowerToys
+winget install --id Microsoft.PowerToys --accept-source-agreements --accept-package-agreements
+
+# Install python 3.10.6 for Stable Diffusion
+winget install --id Python.Python.3.10 --version 3.10.6 --accept-source-agreements --accept-package-agreements
